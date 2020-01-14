7c770f9cff906ab71072d15013749340f7fb3f3d
Billy Cook <cook0318@algonquinlive.com>
2020-01-13 23:45:56 -0500

    added basic game logic in javascript

--- main.js
+++ main.js
@@ -1,1 +1,51 @@
-$("#title").css("border","3px solid blue")
+// $("#title").css("border","3px solid blue")
+
+let options;
+options = ["rock", "paper", "scissors"]
+
+
+function game(choice){
+    let opponent = options[Math.floor(Math.random() * options.length)]
+    if (choice == "rock"){
+        if (opponent == "rock"){
+            console.log(opponent, "draw");
+        } else if (opponent == "scissors"){
+            console.log(opponent, "win");
+        } else if (opponent == "paper") {
+            console.log(opponent, "lose")
+        }
+
+    }
+
+
+
+
+    if (choice == "paper"){
+        if (opponent == "rock"){
+            console.log(opponent, "win");
+        } else if (opponent == "scissors"){
+            console.log(opponent, "lose");
+        } else if (opponent == "paper") {
+            console.log(opponent, "draw")
+        }
+
+    }
+
+
+
+
+    if (choice == "scissors"){
+        if (opponent == "rock"){
+            console.log(opponent, "lose");
+        } else if (opponent == "scissors"){
+            console.log(opponent, "draw");
+        } else if (opponent == "paper") {
+            console.log(opponent, "win")
+        }
+
+    }
+}
+
+
+
+
